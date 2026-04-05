f4a3726d674484bc5e2c04c462a962b1b31a1641
Carl GENNETAIS <carl.gennetais@proton.me>
2026-04-05 14:31:26 +0200

    clean: remove vicinae

--- build/30-apps.sh
+++ build/30-apps.sh
@@ -24,17 +24,17 @@ dnf5 remove -y alacritty
 echo "Ghostty installed"
 echo "::endgroup::"
 
-echo "::group:: Install Vicinae"
-
-# vicinae requires cmark-gfm from a coprdep repo  (quadratech188/cmark-gfm)
-dnf5 -y copr enable quadratech188/vicinae
-dnf5 -y copr disable quadratech188/vicinae
-dnf5 -y install \
-  --enablerepo=copr:copr.fedorainfracloud.org:quadratech188:vicinae \
-  --enablerepo=coprdep:copr.fedorainfracloud.org:quadratech188:cmark-gfm \
-  vicinae
-
-echo "Vicinae installed"
-echo "::endgroup::"
+# echo "::group:: Install Vicinae"
+#
+# # vicinae requires cmark-gfm from a coprdep repo  (quadratech188/cmark-gfm)
+# dnf5 -y copr enable quadratech188/vicinae
+# dnf5 -y copr disable quadratech188/vicinae
+# dnf5 -y install \
+#   --enablerepo=copr:copr.fedorainfracloud.org:quadratech188:vicinae \
+#   --enablerepo=coprdep:copr.fedorainfracloud.org:quadratech188:cmark-gfm \
+#   vicinae
+#
+# echo "Vicinae installed"
+# echo "::endgroup::"
 
 echo "Desktop applications installation complete!"
